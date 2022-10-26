9ff023defc0427e7af6d4e51475955d250e268ae
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-10-25 23:21:35 -0300

    Atualiza Layout incrementa Função

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('Linkado')
+//FORMULÁRIO
+
+const nome = document.getElementById('nomeid')
+const telefone = document.getElementById('foneid')
+const email = document.getElementById('emailid')
+const mensagem = document.getElementById('textid')
+const resultado = document.querySelector('#resultado')
+
+function enviar(){
+    if(nome.value ==='' || telefone.value ==='' || email.value ===''|| mensagem.value ===''){
+      resultado.innerHTML= `<p>Favor preencher todos os campos!</p>`
+    }else{
+     resultado.innerHTML= `<p>Mensagem enviada!</p>`
+    }
+}
