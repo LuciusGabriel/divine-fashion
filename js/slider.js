27a6fbdacc1b002ef431b0dcd8a8c3815b378ff0
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-10-26 21:31:33 -0300

    Adiciona Slider

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+function slide1(){
+    document.getElementById('slider').src="img/001.jpg";
+    setTimeout("slide2()", 5000)
+    }
+    
+    function slide2(){
+    document.getElementById('slider').src="img/002.jpg";
+    setTimeout("slide3()", 5000)
+    }
+    
+    function slide3(){
+    document.getElementById('slider').src="img/003.jpg";
+    setTimeout("slide1()", 5000)
+}
